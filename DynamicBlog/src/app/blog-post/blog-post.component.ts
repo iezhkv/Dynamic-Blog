5f2830b56f3e1c6ded48598a7410e5cada274c4b
iezhkv <lev3el6up9@gmail.com>
2022-11-12 20:46:46 +0200

    Rendered single Post

--- DynamicBlog/src/app/blog-post/blog-post.component.ts
+++ DynamicBlog/src/app/blog-post/blog-post.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { ContentfulService } from '../services/contentful.service';
 
 @Component({
   selector: 'app-blog-post',
@@ -7,9 +10,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BlogPostComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private contentfulService: ContentfulService ) { }
+
+  blogPost$: Observable<any> | undefined;
 
   ngOnInit(): void {
-  }
+    this.route.params.subscribe(
+      params => {
+        const id = params['id'];
+
+        this.blogPost$ = this.contentfulService.getEntryById(id);
+      }
+    )
+  };
 
 }
